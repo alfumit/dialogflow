a208d356e5ac5a0cad54e5a3aa45bfabb456cb84
Alexander Fedotov <alexander.g.fedotov@kaspersky.com>
2018-07-12 18:58:39 +0300

    Added chat components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-// import { ChatModule } from './chat/chat.module';
+import { ChatModule } from './chat/chat.module';
 
 @NgModule({
     declarations: [
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
-        // ChatModule
+        ChatModule
     ],
     providers: [],
     bootstrap: [AppComponent]
