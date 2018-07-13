8fd8beca2a54f2a70e8b89a94a2828da3854c6ba
Alexander Fedotov <alexander.g.fedotov@kaspersky.com>
2018-07-13 19:39:53 +0300

    Adding directline service

--- src/app/react-chat/directline.service.ts
+++ src/app/react-chat/directline.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { DirectLine} from 'botframework-directlinejs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DirectlineService {
+     readonly botConnection = new DirectLine({secret: '', conversationId: ''});
 
   constructor() { }
 }
